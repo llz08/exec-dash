a667f2fda1bb7e3e543a3f370cd847b4d0500f9b
zhanglu08 <65691678+lzhang08@users.noreply.github.com>
2020-06-15 13:38:58 -0400

    import monthly sales report and add csv file path

--- dashboard_generator.py
+++ dashboard_generator.py
@@ -1,13 +1,50 @@
 # dashboard_generator.py
 
+import os
+import csv
+
+def to_usd(my_price):
+  # return "${0:,.2f}".format(my_price)
+  return f"${my_price:,.2f}"
+
+#
+# INFO INPUTS
+#
+
+CSV_FILENAME = "sales-201803.csv"
+
+csv_filepath = os.path.join("data/monthly-sales", CSV_FILENAME)
+print(csv_filepath)
+rows = []
+
+with open(csv_filepath, "r") as csv_file:
+    reader = csv.DictReader(csv_file)
+    for od in reader:
+        rows.append(dict(od)) 
+
+
+sales_prices = [float(row["sales price"]) for row in rows] 
+
+total_sales = sum(sales_prices)
+
+month = "MARCH" # TODO: get from file name or date values
+year = 2018 # TODO: get from file name or date values
+
+#
+# INFO OUTPUTS
+#
+
+
+print("-------------------------")
+print(f"SALES REPORT")
 print("-----------------------")
-print("MONTH: March 2018")
+print(f"MONTH: {month} {year}")
 
 print("-----------------------")
 print("CRUNCHING THE DATA...")
 
 print("-----------------------")
-print("TOTAL MONTHLY SALES: $12,000.71")
+print(f"TOTAL SALES: {to_usd(total_sales)}")
 
 print("-----------------------")
 print("TOP SELLING PRODUCTS:")
